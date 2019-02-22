2591ebb1169e7b026eb26a91f096052d1ad9d353
denebtech <techdeneb@gmail.com>
2019-02-21 20:14:46 -0500

    styled picker

--- src/components/picker.js
+++ src/components/picker.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 
+import 'react-datepicker/dist/react-datepicker.css';
+
 class Picker extends Component {
 
 
