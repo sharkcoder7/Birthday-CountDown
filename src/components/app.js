9d16462d44f11273eb4fa67ebd7a48c68cf178f0
denebtech <techdeneb@gmail.com>
2019-02-21 20:13:59 -0500

    installed moment and datepicker, placed picker component on grid with datepicker on it

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Picker from './picker';
 
 export default class App extends Component {
   render() {
@@ -12,6 +13,8 @@ export default class App extends Component {
         <div className="grid__skew-light-one"></div>
         <div className="grid__skew-light-two"></div>
         <div className="grid__skew-light-three-box"></div>
+
+        <Picker/>
       </div>
     );
   }
---
+++ src/components/picker.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
+class Picker extends Component {
+
+
+    constructor (props) {
+        super(props)
+        this.state = {
+            startDate: moment()
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(date) {
+         console.log('trying to change date for', date_d)
+        this.setState({
+            startDate: date
+        });
+    }
+    render () {
+        return (
+            <div className="picker">
+                <DatePicker
+                    selected={this.state.startDate}
+                    onChange={this.handleChange}
+                />
+            </div>
+        )
+    }
+}
+
+export default Picker;
