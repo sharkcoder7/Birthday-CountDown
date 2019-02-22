1a6f63c7e0139111cc241d48fe5542f5cac39a9d
denebtech <techdeneb@gmail.com>
2019-02-21 20:12:43 -0500

    added birthday countdown title

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,11 @@ export default class App extends Component {
   render() {
     return (
       <div className="grid">
-        <div>hi</div>
-        <div>hi</div>
-        <div>hi</div>
+        <h1 className="grid__title">Birthday Countdown</h1>
+        <div className="grid__skew-dark-one-box"></div>
+        <div className="grid__skew-dark-two"></div>
+        <div className="grid__skew-dark-three"></div>
+
         <div className="grid__skew-light-one"></div>
         <div className="grid__skew-light-two"></div>
         <div className="grid__skew-light-three-box"></div>
